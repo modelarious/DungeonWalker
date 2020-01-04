21684984ed22bff94943ac05b531e77f68a09374
modelarious <michaelwilliamhackman@gmail.com>
2020-01-03 21:41:57 -0700

    Got the collision checks working as well as adding exceptions

--- generateMap.py
+++ generateMap.py
@@ -55,16 +55,65 @@ charSet = {
 #       "Lava" : "L"
 }
 
+# thrown when given room dimensions are too large
+class RoomTooLarge(Exception):
+	pass
+
+# thrown when given room dimensions are too small
+class RoomTooSmall(Exception):
+	pass
+
+# thrown when a room given to the board would be outside the boundaries
+class RoomOutsideBoard(Exception):
+	pass
+
+# thrown when a room given to the board collides with another room
+class RoomCollision(Exception):
+	pass
+
+# thrown when the board 
+class BoardTooSmall(Exception):
+	pass
+
+
 class Room(object):
+	MAX_ROOM_HEIGHT = 10
+	MAX_ROOM_WIDTH = 10
+	MIN_ROOM_WIDTH = 2
+	MIN_ROOM_HEIGHT = 2
+
 	def __init__(self, height, width, topLeftX, topLeftY):
+
+		#check if the room is too large
+		if height > Room.MAX_ROOM_HEIGHT: raise RoomTooLarge
+		if width > Room.MAX_ROOM_WIDTH: raise RoomTooLarge
+
+		#check if the room is too small
+		if height < Room.MIN_ROOM_HEIGHT: raise RoomTooSmall
+		if width < Room.MIN_ROOM_WIDTH: raise RoomTooSmall
+
 		self.height = height
 		self.width = width
-		self.topLeftX = topLeftX
-		self.topLeftY = topLeftY
+
+		self.leftX = topLeftX
+		self.rightX = topLeftX + width
+		self.topY = topLeftY
+		self.bottomY = topLeftY + height
 
 
-		#self.bottomRightX = topLeftX + 
-		
+	#checks if there are any gaps between any edges on the boxes, 
+	#if there aren't, then there is a collision
+	def collide(self, otherRoom):
+		conds = [self.leftX < otherRoom.rightX,
+			self.rightX > otherRoom.leftX,
+			self.topY < otherRoom.bottomY,
+			self.bottomY > otherRoom.topY]
+		return all(conds)
+
+	def __str__(self):
+		return f"leftX = {self.leftX}, rightX = {self.rightX}, topY = {self.topY}, bottomY = {self.bottomY}"
+
+
 class Board(object):
 	def __init__(self, width, height):
 		assert width >= 4, f"Width must be 4 or greater, not {width}"
@@ -86,7 +135,6 @@ class Board(object):
 			for x in range(self.width):
 				row.append(charSet["blocked"])
 			board.append(row)
-		print(board)
 		self.board = board
 
 	#print the board to the screen
@@ -95,33 +143,67 @@ class Board(object):
 			print("".join(row))
 		print()
 	
-	#returns False on failure:
-	#	- height or width were less than 2
-	#	- top left X or top left Y were less than 1
-	#returns True on success
+	#raises exceptions for cases where:
+	#	- the room would leave the bounds of the board
+	#	- the room would collide with another that already exists
 	def addRoom(self, room):
-		#if height or width are < 2 or the 
-		#top left coordinates are < 1, return false
-		if room.height < 2 or room.width < 2 or room.topLeftX < 1 or room.topLeftY < 1:
-			return False
 
-		#XXX if the rectangle would go out of bounds on the board
+		#if the rectangle would leave the bounds of the board
+		if room.rightX > self.width - 1 or room.leftX < 1 or room.bottomY > self.height -1 or room.topY < 1:
+			raise RoomOutsideBoard
 
-		#XXX if the room would collide with another
+		#if the room would collide with another that has already been placed
+		if any(room.collide(placedRoom) for placedRoom in self.rooms):
+			raise RoomCollision
 
-		for x in range(room.topLeftX, room.topLeftX + room.width):
-			for y in range(room.topLeftY, room.topLeftY + room.height):	
+		for x in range(room.leftX, room.rightX):
+			for y in range(room.topY, room.bottomY):	
 				self.board[y][x] = charSet["passable"]
 
+		self.rooms.append(room)
 		return True
-	
+
+
+
+# thrown when a room given to the board would be outside the boundaries
+class RoomOutsideBoard(Exception):
+        pass
+
+# thrown when a room given to the board collides with another room
+class RoomCollision(Exception):
+        pass
+
+# thrown when the board
+class BoardTooSmall(Exception):
+        pass	
 x = Board(12, 12)
 
-roomHeight, roomWidth = (3, 4)
+'''
+`````````````
+`***`````````
+`***`````````
+`***`````***`
+`````````***`
+`````````***`
+`````````````
+```****``````
+```****``````
+```****``````
+`````````````
+'''
+
+x.addRoom(Room(3, 3, 1, 1))
+x.addRoom(Room(3, 3, 8, 4))
+x.addRoom(Room(3, 4, 3, 7))
+'''
+roomHeight, roomWidth = (4, 5)
 topLeftX, topLeftY = (3,1)
 
 r = Room(roomHeight, roomWidth, topLeftX, topLeftY)
 x.addRoom(r)
+
+x.addRoom(r)
+'''
 x.drawBoard()
 
 
