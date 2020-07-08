94e6c8b6656b277efe57924b4a7b5a5962f4b6be
Michael Hackman <mwhackma@ualberta.ca>
2020-07-07 18:03:29 -0600

    pass grid size into the enemy factory

--- factories/EnemyControllerFactory.py
+++ factories/EnemyControllerFactory.py
@@ -4,12 +4,13 @@ from models.CharacterModel import CharacterModel
 from random import randint
 
 class EnemyControllerFactory:
-    def __init__(self, mapController, playerController):
+    def __init__(self, mapController, playerController, grid_size):
         self.mapController = mapController
         self.playerController = playerController
+        self.grid_size = grid_size
     
     def get_enemy(self, spawnCoords):
         characterModel = CharacterModel(*spawnCoords)
         RED = (255, 0, 0)
-        characterView = CharacterView(grid_size=32, color=RED)
+        characterView = CharacterView(grid_size=self.grid_size, color=RED)
         return EnemyController(characterView, characterModel, self.mapController, self.playerController)
