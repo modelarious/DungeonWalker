e860d8862c57c4238254147e3c604d9079c4723a
Michael Hackman <mwhackma@ualberta.ca>
2020-07-15 00:45:04 -0600

    everthing moved into class

--- TileMapper/TileMapper.py
+++ TileMapper/TileMapper.py
@@ -12,11 +12,18 @@ from time import sleep
 #             box = (j*width, i*height, (j+1)*width, (i+1)*height)
 #             yield im.crop(box)
 
-def get_stairs_one(image):
-    top = 387
-    left = 13
-    grid_size = 24
-    return crop_and_resize_square_image(image, top, left, grid_size)
+
+# def get_stairs_one(image):
+#     top = 387
+#     left = 13
+#     grid_size = 24
+#     return crop_and_resize_square_image(image, top, left, grid_size)
+
+# def get_stairs_two(image):
+#     top = 387
+#     left = 13
+#     grid_size = 24
+#     return crop_and_resize_square_image(image, top, left, grid_size)
 
 from enum import Enum
 class TileType(Enum):
@@ -38,15 +45,27 @@ class TilePosition(Enum):
 # images are not hashable because they are mutable, so this is my solution that allows a hashmap
 # to index an array of tiles instead
 # XXX all the functionality in this file could likely fit in this class...
-class TileManager:
-    def __init__(self, tilesetImage):
+class TileLoader:
+    def __init__(self, tilesetImage, tileTypeToColumnNumberAssignments):
         self.tiles = []
         self.tileIndex = {} # string -> index_to_tiles_array
         self.image = tilesetImage
 
+        self.grid_size = 24 # each of the square tiles are 24 pixels wide
+        self.border = 1 # there is a 1 pixel border around each of the individual square tiles. This border is shared (so the right border of one tile is the left border of the adjacent tile)
+        self.scale_factor = self.grid_size + self.border # so to jump to the next tile, you need to move the tile width (grid_size) + the size of the shared border (border)
+
+        self._populate_data(tileTypeToColumnNumberAssignments)
+
+
+    def _populate_data(self, tileTypeToColumnNumberAssignments):
         # pre populate data container with blank objects
         for tileType in TileType:
             self.tileIndex[tileType] = {}
+        
+        # populate the requested tile types from the requested columns
+        for tileType, columnNumber in tileTypeToColumnNumberAssignments.items():
+            self._populate_from_column(tileType, columnNumber)
     
     def add_tile(self, tileType, tilePosition, tile):
         self.tileIndex[tileType][tilePosition] = len(self.tiles)
@@ -65,79 +84,67 @@ class TileManager:
         return pygameTile
 
     # fetches the legend from the file
-    # XXX THIS SHOULD TAKE A column number and a tileType
-    def populate_from_column(self, tileType, columnNumber):
+    def _populate_from_column(self, tileType, columnNumber):
 
         # XXX these could likely be member variables
         top = 162
         left = 8
-        grid_size = 24 # each of the square tiles are 24 pixels wide
-        border = 1 # there is a 1 pixel border around each of the individual square tiles. This border is shared (so the right border of one tile is the left border of the adjacent tile)
 
-        scale_factor = grid_size + border # so to jump to the next tile, you need to move the tile width (grid_size) + the size of the shared border (border)
-
-        # for testing, move over to the Walls column
-        left += scale_factor * 3 * columnNumber
+        # move to the requested column
+        left += self.scale_factor * 3 * columnNumber
 
         # get first 3x3 of values (standard pieces)
-        tiles = self.get_three_by_three_tile_matrix(self.image, left, top, grid_size, scale_factor, border)
+        tiles = self.get_three_by_three_tile_matrix(left, top)
         for t, pos in zip(tiles, TilePosition):
             self.add_tile(tileType, pos, t)
 
         # skip down to the next 3x3 of tiles
-        # top += scale_factor * 3
-
-        # for testing, move over to the Ground column
-        # left += scale_factor * 3 * 3
+        top += self.scale_factor * 3
+        # XXX fetch more advanced pieces (going to need a reflection algo for some tiles, and ability to ignore empty tiles that haven't been reflected)
     
-    # XXX doesn't need a bunch of those parameters
-    def get_three_by_three_tile_matrix(self, image, leftX, topY, grid_size, scale_factor, border):
+    def get_three_by_three_tile_matrix(self, leftX, topY):
 
         # ignore the border on the left and top
-        startX = leftX + border
-        startY = topY + border
+        startX = leftX + self.border
+        startY = topY + self.border
 
         tiles = []
 
         # range to 3*scale_factor because we are grabbing 3 squares
-        for y_dim in range(startY, startY + 3*scale_factor, scale_factor):
-            for x_dim in range(startX, startX + 3*scale_factor, scale_factor):
-                tile = crop_and_resize_square_image(image, y_dim, x_dim, grid_size)
+        for y_dim in range(startY, startY + 3*self.scale_factor, self.scale_factor):
+            for x_dim in range(startX, startX + 3*self.scale_factor, self.scale_factor):
+                tile = self.crop_and_resize_square_image(y_dim, x_dim)
                 tiles.append(tile)
         
         return tiles
 
-# def get_stairs_two(image):
-#     top = 387
-#     left = 13
-#     grid_size = 24
-#     return crop_and_resize_square_image(image, top, left, grid_size)
-
-def crop_and_resize_square_image(im, top, left, grid_size):
-    right = left + grid_size
-    bottom = top + grid_size
-    box = (left, top, right, bottom)
-    cropped = im.crop(box)
-    desiredSize = (32, 32)
-    resized = cropped.resize(desiredSize)
-    return resized
+    def crop_and_resize_square_image(self, top, left):
+        right = left + self.grid_size
+        bottom = top + self.grid_size
+        box = (left, top, right, bottom)
+        cropped = self.image.crop(box)
+        desiredSize = (32, 32)
+        resized = cropped.resize(desiredSize)
+        return resized
 
 infile2 = "assets/tilesets/world abyss.png"
 with Image.open(infile2) as i:
-    tileManager = TileManager(i)
-    tileManager.populate_from_column(TileType.WALL, 1) # wall tiles are column 1
-    tileManager.populate_from_column(TileType.GROUND, 4) # ground tiles are column 4
+    tileTypeToColumnNumberAssignments = {
+        TileType.WALL: 1,
+        TileType.GROUND: 4
+    }
+    tileLoader = TileLoader(i, tileTypeToColumnNumberAssignments)
 
     game_screen = pygame.display.set_mode((1024, 768))
     while True:
         for event in pygame.event.get():
-            tt = TileType.WALL
+            tt = TileType.GROUND
             x = 0
             y = 0
 
             # XXX what this has taught us is that our TilePosition is defined in the wrong order
             for tp in TilePosition:
-                t = tileManager.get_tile(tt, tp)
+                t = tileLoader.get_tile(tt, tp)
                 game_screen.blit(t, (x, y))
                 if x == 64:
                     x = 0
