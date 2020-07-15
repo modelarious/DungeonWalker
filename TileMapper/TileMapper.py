22891d5f73e2d92fbd8def5402ae2f5f5ec33f73
Michael Hackman <mwhackma@ualberta.ca>
2020-07-14 22:26:31 -0600

    got a tile drawing to the screen

--- TileMapper/TileMapper.py
+++ TileMapper/TileMapper.py
@@ -1,14 +1,15 @@
 from PIL import Image
 import pygame
+from time import sleep
 
-def export_for_pygame(tilesetHash):
+def export_for_pygame(tilesetArray):
     strFormat = 'RGBA'
-    exported = {}
-    for description, tile in tilesetHash:
+    exported = []
+    for tile in tilesetArray:
 
         # https://riptutorial.com/pygame/example/21220/using-with-pil fetched on July 13, 2020
-        raw_str = tile.tostring("raw", strFormat)
-        exported[description] = pygame.image.fromstring(raw_str, tile.size, strFormat)
+        raw_bytes = tile.tobytes("raw", strFormat)
+        exported.append(pygame.image.fromstring(raw_bytes, tile.size, strFormat))
     return exported
 
 # def crop(infile,height,width):
@@ -23,7 +24,7 @@ def get_stairs_one(image):
     top = 387
     left = 13
     grid_size = 24
-    return crop_square_image(image, top, left, grid_size)
+    return crop_and_resize_square_image(image, top, left, grid_size)
 
 def get_three_by_three_tile_matrix(image, leftX, topY, grid_size, scale_factor, border):
 
@@ -36,7 +37,7 @@ def get_three_by_three_tile_matrix(image, leftX, topY, grid_size, scale_factor, 
     # range to 3*scale_factor because we are grabbing 3 squares
     for x_dim in range(startX, startX + 3*scale_factor, scale_factor):
         for y_dim in range(startY, startY + 3*scale_factor, scale_factor):
-            tile = crop_square_image(image, y_dim, x_dim, grid_size)
+            tile = crop_and_resize_square_image(image, y_dim, x_dim, grid_size)
             tiles.append(tile)
     
     return tiles
@@ -49,17 +50,28 @@ class TileManager:
         self.tiles = []
         self.tileIndex = {} # string -> index_to_tiles_array
     
-    def add_tile(self, tile, title):
+    def add_tile(self, title, tile):
         self.tileIndex[title] = len(self.tiles)
         self.tiles.append(tile)
     
     def get_tile(self, title):
         arrayIndex = self.tileIndex[title]
         return self.tiles[arrayIndex]
+    
+    # XXX yuck, make it call this function on each element as it's added
+    def finalize_tiles(self):
+        self.tiles = export_for_pygame(self.tiles)
+        
+# class TileLoader:
+#     def __init__(self, filename, tileAccumulator):
+#         self.filename = filename
+#         self.tileAccumulator = tileAccumulator
+    
 
 
 # fetches the legend from the file
 def get_first_column(image):
+    TM = TileManager()
     top = 162
     left = 8
     grid_size = 24 # each of the square tiles are 24 pixels wide
@@ -73,6 +85,7 @@ def get_first_column(image):
     # get first 3x3 of values
     tiles = get_three_by_three_tile_matrix(image, left, top, grid_size, scale_factor, border)
     for t in tiles:
+        TM.add_tile("farts", t)
         t.show()
 
     # skip down to the next 3x3 of tiles
@@ -82,28 +95,35 @@ def get_first_column(image):
     for t in ground:
         t.show()
     
-    return {tileCount: element for element, tileCount in enumerate(ground)}
+    return TM
 
 # def get_stairs_two(image):
 #     top = 387
 #     left = 13
 #     grid_size = 24
-#     return crop_square_image(image, top, left, grid_size)
+#     return crop_and_resize_square_image(image, top, left, grid_size)
 
-def crop_square_image(im, top, left, grid_size):
+def crop_and_resize_square_image(im, top, left, grid_size):
     right = left + grid_size
     bottom = top + grid_size
     box = (left, top, right, bottom)
-    return im.crop(box)
-
-
-
-# infile = "assets/tilesets/stairs.png"
-# with Image.open(infile) as i:
-#     stairs1 = get_stairs_one(i)
-#     stairs1.show()
+    cropped = im.crop(box)
+    desiredSize = (32, 32)
+    resized = cropped.resize(desiredSize)
+    return resized
 
 infile2 = "assets/tilesets/world abyss.png"
 with Image.open(infile2) as i:
-    get_first_column(i)
-    
+
+    # XXX GROSS!!!!!!! Don't modify the tileManager inside a function.... make this whole thing into
+    # a class, so you don't have to do dirty deeds
+    tileManager = TileManager()
+    TM = get_first_column(i)
+    TM.finalize_tiles()
+
+    game_screen = pygame.display.set_mode((1024, 768))
+    while True:
+        for event in pygame.event.get():
+            t = TM.get_tile("farts")
+            game_screen.blit(t, t.get_rect())
+            pygame.display.flip()
