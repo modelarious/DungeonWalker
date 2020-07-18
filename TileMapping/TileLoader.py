d366fd48c8554429d127b24a832a782c4c204f72
Michael Hackman <mwhackma@ualberta.ca>
2020-07-18 17:18:20 -0600

    checkpoint

--- TileMapping/TileLoader.py
+++ TileMapping/TileLoader.py
@@ -3,37 +3,212 @@ from TileMapping.TilePosition import TilePosition
 import pygame
 from PIL import Image
 
+
+class TileContainer:
+    ''' 
+    keep associated pieces of data together, and make it less brittle to 
+    change parts of the code that return this class 
+    '''
+    def __init__(self):
+        self.tiles = []
+        self.coords = []
+    
+    def add_tile(self, tile, coord):
+        self.tiles.append(tile)
+        self.coords.append(coord)
+    
+    def get_tiles_and_coords(self):
+        return self.tiles, self.coords
+
+class Loader:
+    ''' shared tools that can be used by any class that loads from a tileset image '''
+    def __init__(self, tilesetImage, gameGridSize):
+        # useful local constants
+        self.gameGridSize = gameGridSize # the grid_size we are using for display
+        self.image = tilesetImage
+        self.tileSetGridSize = 24 # each of the square tiles on the image are 24 pixels wide
+        self.border = 1 # there is a 1 pixel border around each of the individual square tiles in the image. This border is shared (so the right border of one tile is the left border of the adjacent tile)
+        self.scaleFactor = self.tileSetGridSize + self.border # so to jump to the next tile, you need to move the tile width (tileSetGridSize) + the size of the shared border (border)
+
+    def _get_n_by_n_tile_matrix(self, leftX, topY, n):
+
+        # ignores the border on the left and top
+        startX = leftX + self.border
+        startY = topY + self.border
+
+        tiles, coordinates = [], []
+
+        # example, get 3x3 -> range to startY + 3*scaleFactor because we are grabbing 3 squares starting from startY
+        # XXX why y before x?
+        for yDim in range(startY, startY + n*self.scaleFactor, self.scaleFactor):
+            for xDim in range(startX, startX + n*self.scaleFactor, self.scaleFactor):
+                tile = self._crop_and_resize_square_image(yDim, xDim)
+                tiles.append(tile)
+                coordinates.append((xDim, yDim))
+
+        return tiles, coordinates
+
+    def _crop_and_resize_square_image(self, top, left):
+        right = left + self.tileSetGridSize
+        bottom = top + self.tileSetGridSize
+        box = (left, top, right, bottom)
+        cropped = self.image.crop(box)
+        desiredSize = (self.gameGridSize, self.gameGridSize)
+        resized = cropped.resize(desiredSize)
+        return resized
+
+from enum import Enum
+class TileSimilarity(Enum):
+    Different = 0
+    Same = 1
+
+class Colors(Enum):
+    TEAL=(0, 128, 128)
+    BLACK=(1, 1, 1)
+    WHITE=(254, 254, 254)
+
+class TemplateTileEmpty(Exception):
+    pass
+
+Different = TileSimilarity.Different
+Same = TileSimilarity.Same
+
+class TemplateLoader(Loader):
+    def __init__(self, tilesetImage, gameGridSize):
+        super().__init__(tilesetImage, gameGridSize)
+
+        #we don't want to resize the tiles to fit the grid when loading the template
+        # XXX the fact that we have to blindly do this feels like bad design. Make 
+        # the _get_n_by_n_tile_matrix() function take a resize factor and then you 
+        # won't have to overwrite this here
+        self.gameGridSize = self.tileSetGridSize
+
+    def parse_template_column(self):
+        # XXX these could likely be member variables
+        top = 162
+        left = 8
+
+        output = []
+
+        # get first 3x3 of values
+        tileCountInRow = 3
+        tiles, coords = self._get_n_by_n_tile_matrix(left, top, tileCountInRow)
+        for tile, pos in zip(tiles, coords):
+            try:
+                threeByThreeMatrixKey = self._calculate_three_by_three_matrix_template_key(tile)
+                output.append( (pos, threeByThreeMatrixKey) )
+            except TemplateTileEmpty:
+                print("tile was empty!!")
+                # the template doesn't have any information for this tile (it's not in the tileset)
+                pass
+
+        from pprint import pprint
+        print()
+        print()
+        print()
+        print()
+        pprint(output)
+
+        return output
+
+    '''
+exampleKey = (
+  [Different, Different, Different],
+  [Same,      Same,      Different],
+  [Same,      Same,      Different]
+)
+    '''
+    def _calculate_three_by_three_matrix_template_key(self, tile):
+        tileColorMatrix = self._get_tile_color_three_by_three_matrix(tile)
+
+        # If center tile in 3x3 2d array is not white, raise TemplateTileEmpty.
+        # This is because this tile in the template is unused by the image file.
+        if tileColorMatrix[1][1] != Colors.WHITE.value:
+            raise TemplateTileEmpty
+
+        from pprint import pprint
+        pprint(tileColorMatrix)
+        threeByThreeMatrixKey = self._turn_color_matrix_into_key(tileColorMatrix)
+        pprint(threeByThreeMatrixKey)
+        return threeByThreeMatrixKey
+
+    def _turn_color_matrix_into_key(self, tileColorMatrix):
+        threeByThreeMatrixKey = []
+        # XXX this needs to be done
+        for row in tileColorMatrix:
+            outRow = []
+            for color in row:
+                # if the space was different from the center space in the template, it will be teal
+                if color == Colors.TEAL.value:
+                    outRow.append(Different)
+                else:
+                    outRow.append(Same)
+            threeByThreeMatrixKey.append(tuple(outRow))
+        
+            
+
+
+        
+        return ((Different, Different, Different), (Same, Same, Different), (Same, Same, Different))
+
+    # works for any "n", but only used for three by three calculations
+    # processing a tile in the template to generate a key for fast fetching later
+    def _get_tile_color_three_by_three_matrix(self, tile):
+        tileCountInRow=3
+
+        rgbTile = tile.convert('RGB')
+
+        innerTemplateTileSize = int(self.tileSetGridSize / tileCountInRow)
+        firstInnerTileCenter = int(innerTemplateTileSize - 0.5 * innerTemplateTileSize)
+        scaleFactor = innerTemplateTileSize # jump this many pixels to get to the center of the next tile
+
+        tileColorMatrix = []
+        # example, get 3x3 -> range to startY + 3*scaleFactor because we are grabbing 3 squares starting from startY
+        # XXX why y before x?
+        xCenters = range(firstInnerTileCenter, firstInnerTileCenter + tileCountInRow*scaleFactor, scaleFactor)
+        yCenters = range(firstInnerTileCenter, firstInnerTileCenter + tileCountInRow*scaleFactor, scaleFactor)
+        for yDim in yCenters:
+            row = []
+            for xDim in xCenters:
+                rgbValue = rgbTile.getpixel((xDim, yDim))
+                row.append(rgbValue)
+            tileColorMatrix.append(row)
+        
+        return tileColorMatrix
+
+
 # images are not hashable because they are mutable, so this is my solution that allows a hashmap
 # to index an array of tiles instead
-class TileLoader:
-    def __init__(self, tilesetImage, grid_size, tileTypeToColumnNumberAssignments):
+class TileLoader(Loader):
+    def __init__(self, tilesetImage, gameGridSize, tileTypeToColumnNumberAssignments, templateLoader):
+        super().__init__(tilesetImage, gameGridSize)
         # data structure
         self.tiles = []
         self.tileIndex = {} # string -> index_to_tiles_array
-        
-        # useful local constants
-        self.grid_size = grid_size # the grid_size we are using for display
-        self.image = tilesetImage
-        self.tile_set_grid_size = 24 # each of the square tiles on the image are 24 pixels wide
-        self.border = 1 # there is a 1 pixel border around each of the individual square tiles in the image. This border is shared (so the right border of one tile is the left border of the adjacent tile)
-        self.scale_factor = self.tile_set_grid_size + self.border # so to jump to the next tile, you need to move the tile width (tile_set_grid_size) + the size of the shared border (border)
+
+        self.templateLoader = templateLoader
 
         # perform load of the tileset into the data structures
         self._populate_data(tileTypeToColumnNumberAssignments)
 
-    def get_tile(self, tileType, tilePosition):
-        arrayIndex = self.tileIndex[tileType][tilePosition]
+    def get_tile(self, tileType, tileNeighborSettings):
+        arrayIndex = self.tileIndex[tileType][tileNeighborSettings]
         return self.tiles[arrayIndex]
 
     # perform load of the tileset into the data structures
-    def _populate_data(self, tileTypeToColumnNumberAssignments):        
+    def _populate_data(self, tileTypeToColumnNumberAssignments):
+
+        # key: value mapping where the key is the arrangement of columns as discussed 
+        # here: https://github.com/modelarious/DungeonWalker/issues/64#issuecomment-660400003
+        template_column = self._parse_template_column()
+        
         # populate the requested tile types from the requested columns
         for tileType, columnNumber in tileTypeToColumnNumberAssignments.items():
             self.tileIndex[tileType] = {}
             self._populate_from_column(tileType, columnNumber)
     
-    def _add_tile(self, tileType, tilePosition, tile):
-        self.tileIndex[tileType][tilePosition] = len(self.tiles)
+    def _add_tile(self, tileType, tile, tileNeighborSettings):
+        self.tileIndex[tileType][tileNeighborSettings] = len(self.tiles)
         self.tiles.append(self._export_for_pygame(tile))
 
     # translate from PIL to pygame so the tile can be drawn to the screen
@@ -43,6 +218,9 @@ class TileLoader:
         rawBytesTile = tile.tobytes("raw", strFormat)
         pygameTile = pygame.image.fromstring(rawBytesTile, tile.size, strFormat)
         return pygameTile
+    
+    def _parse_template_column(self):
+        return self.templateLoader.parse_template_column()
 
     # fetches the legend from the file
     def _populate_from_column(self, tileType, columnNumber):
@@ -51,39 +229,17 @@ class TileLoader:
         top = 162
         left = 8
 
-        # move to the requested column
-        left += self.scale_factor * 3 * columnNumber
+        # move to the requested column. * 3 because the scaleFactor applies to 
+        # single tiles and we need to jump over 3 of them to get to the next column.
+        # therefore we need to jump over 3 * columnNumber to get to columnNumber
+        left += self.scaleFactor * 3 * columnNumber
 
         # get first 3x3 of values (standard pieces)
-        tiles = self._get_three_by_three_tile_matrix(left, top)
-        for t, pos in zip(tiles, TilePosition):
+        tiles = self._get_n_by_n_tile_matrix(left, top, 3)
+        for t, pos in zip(tiles, TileNeighborSettings):
             self._add_tile(tileType, pos, t)
 
         # skip down to the next 3x3 of tiles
-        top += self.scale_factor * 3
+        top += self.scaleFactor * 3
         # XXX fetch more advanced pieces (going to need a reflection algo for some tiles, and ability to ignore empty tiles that haven't been reflected)
     
-    def _get_three_by_three_tile_matrix(self, leftX, topY):
-
-        # ignore the border on the left and top
-        startX = leftX + self.border
-        startY = topY + self.border
-
-        tiles = []
-
-        # range to 3*scale_factor because we are grabbing 3 squares
-        for y_dim in range(startY, startY + 3*self.scale_factor, self.scale_factor):
-            for x_dim in range(startX, startX + 3*self.scale_factor, self.scale_factor):
-                tile = self._crop_and_resize_square_image(y_dim, x_dim)
-                tiles.append(tile)
-        
-        return tiles
-
-    def _crop_and_resize_square_image(self, top, left):
-        right = left + self.tile_set_grid_size
-        bottom = top + self.tile_set_grid_size
-        box = (left, top, right, bottom)
-        cropped = self.image.crop(box)
-        desiredSize = (self.grid_size, self.grid_size)
-        resized = cropped.resize(desiredSize)
-        return resized
--- factories/MapControllerFactory.py
+++ factories/MapControllerFactory.py
@@ -6,6 +6,7 @@ from factories.MapModelFactory import MapModelFactory
 
 from TileMapping.TileMapper import TileMapper
 from TileMapping.TileMapper import TileLoader
+from TileMapping.TileLoader import TemplateLoader
 from PIL import Image
 from TileMapping.TileType import TileType
 from views.TileMappedView import TileMappedView
@@ -21,8 +22,8 @@ class MapControllerFactory(FactoryBaseClass):
 		# XXX RandomMapGenerationDriver instantiation is weird, should make it so 
 		# that you pass in RandomMapGenerationDriver(), then register the map with it
 		mapModelFactory = MapModelFactory(
-			self.get_copy(self.max_x_tiles), 
-			self.get_copy(self.max_y_tiles), 
+			self.max_x_tiles, 
+			self.max_y_tiles, 
 			RandomMapGenerationDriver
 		)
 		mapModel = mapModelFactory.generate_new_map()
@@ -30,22 +31,21 @@ class MapControllerFactory(FactoryBaseClass):
 		# XXX this is a lot of baggage for one factory
 		# and a lot is related to tile mapping, so make that into a separate factory
 		infile2 = "assets/tilesets/world abyss.png"
-		with Image.open(infile2) as i:
+		with Image.open(infile2) as im:
+
+			# this could be changed to an array of objects that contain a tileType and a column number
 			tileTypeToColumnNumberAssignments = {
 				TileType.WALL: 1,
 				TileType.GROUND: 4
 			}
-			tileLoader = TileLoader(i, self.get_copy(self._grid_size), tileTypeToColumnNumberAssignments)
+			templateLoader = TemplateLoader(im, self._grid_size)
+			tileLoader = TileLoader(im, self._grid_size, tileTypeToColumnNumberAssignments, templateLoader)
 
 		tileMapper = TileMapper(tileLoader)
-
 		tileMapper.process_board(mapModel)
+		tileMapperView = TileMappedView(self._grid_size, tileMapper)
 
-		tileMapperView = TileMappedView(self.get_copy(self._grid_size), tileMapper)
-
-
-
-		mapView = MapView(self.get_copy(self._grid_size), tileMapperView)
+		mapView = MapView(self._grid_size, tileMapperView)
 
 		return MapController(mapModel, mapView, mapModelFactory, tileMapper)
 
