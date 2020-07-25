afa3388e731c40fffd851b9cd76d90daf84afd43
Michael Hackman <mwhackma@ualberta.ca>
2020-07-24 20:20:36 -0600

    checkpoint before applying state pattern

--- controllers/mvc/EnemyController.py
+++ controllers/mvc/EnemyController.py
@@ -11,6 +11,16 @@ from models.CharacterModel import CharacterModel
 from views.CharacterView import CharacterView
 from controllers.mvc.PlayerController import PlayerController
 
+class AIUnit:
+	def __init__(self):
+		self.blah = 0
+	
+
+class FivePlyLookaheadAIUnit(AIUnit):
+	def __init__(self):
+		self.bleh = 3
+
+
 
 # XXX might be better not to give the enemy controller the player controller but instead an object that
 # wraps the player model and lets the enemy make queries about the player 
@@ -27,8 +37,23 @@ class EnemyController(CharacterController):
 		directions = [Right(), Left(), Up(), Down(), NullMove()]
 		shuffle(directions)
 
+
+		# XXX STATE PATTERN PLEEEEEEASE
+		# if the enemy is far away from the player, just move randomly
 		playerPosition = self.playerController.get_pos()
-		minDistToPlayer = 500
+		if manhatten_distance(*playerPosition, *self._characterModel.get_pos()) > 7:
+			for move in directions:
+				if self.movement_valid(move) and not self.movement_prevented(move, preventedPositions):
+					self._characterModel.move(move)
+					return True
+
+			# didn't find a move that worked
+			return False
+
+		# keep track of the minimum distance to the player
+		# pick the move that gets the enemy closest
+		infinity = 999999999
+		minDistToPlayer = infinity
 		selectedMovement = None
 		for move in directions:
 			if self.movement_valid(move) and not self.movement_prevented(move, preventedPositions):
