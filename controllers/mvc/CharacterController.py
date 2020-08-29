b086fa8e2d3ea241d6c030caccdfc5f8351c372c
Michael Hackman <mwhackma@ualberta.ca>
2020-08-28 20:18:59 -0600

    this code is now unneeded

--- controllers/mvc/CharacterController.py
+++ controllers/mvc/CharacterController.py
@@ -21,17 +21,3 @@ class CharacterController(ControllerBaseClass):
 	# XXX this should be abolished!
 	def get_pos(self):
 		return self._characterModel.get_pos()
-	
-	# prevent the player from moving to a invalid space
-	# XXX also not how a controller should be used. 
-	# XXX Move the "movement_valid" and "movement_prevented" functions to
-	# XXX a separate class
-	def movement_valid(self, direction, characterModel):
-		orig_pos = characterModel.get_pos()
-		speculative_new_player_pos = characterModel.get_speculative_position(direction)
-		return self._mapModel.is_legal_move(orig_pos, speculative_new_player_pos)
-	
-	def movement_prevented(self, direction, preventedPositions):
-		# XXX GROSS, ask the model!!!
-		speculative_new_player_pos = self._characterModel.get_speculative_position(direction)
-		return speculative_new_player_pos in preventedPositions
