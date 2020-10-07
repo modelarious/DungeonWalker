aed299721d7b5c4f0c29f7a131e7e84b9663955d
Michael Hackman <mwhackma@ualberta.ca>
2020-10-07 07:36:34 -0600

    just need to scale up the tiles in the tilemapper now

--- views/MapView.py
+++ views/MapView.py
@@ -15,13 +15,13 @@ class MapView(ViewBaseClass):
 		grid_size = self._grid_size
 		cameraBox = self._camera.get_camera_box(mapModel)
 
-		self._tileMapperView.updateView(game_screen, *cameraBox)
 		
+
 		y = 0
 		for row in mapModel.get_part_of_board(*cameraBox):
 			x = 0
 			for tile in row:
-				tile.draw_pygame_representation(game_screen, x % 1025, (x + grid_size) % 1025, y % 769, (y + grid_size)% 769) #XXX
+				# tile.draw_pygame_representation(game_screen, x % 1025, (x + grid_size) % 1025, y % 769, (y + grid_size)% 769) #XXX
 				x += grid_size
 			y += grid_size
 			
@@ -34,5 +34,7 @@ class MapView(ViewBaseClass):
 				.replace(charSet["anchor"], charSet["passable"])
 			)
 		print()
+
+		self._tileMapperView.updateView(game_screen, *cameraBox)
 
 		
--- views/TileMappedView.py
+++ views/TileMappedView.py
@@ -8,11 +8,14 @@ class TileMappedView(ViewBaseClass):
 		self.grid_size = grid_size
 		
 	def updateView(self, game_screen, minX, maxX, minY, maxY):
+		xDelta = maxX - minX
+		yDelta = maxY - minY
 
 		for point, tile in self._tileMapper.get_tile_mapping():
 			x, y = point
 			if x < minX or x >= maxX or y < minY or y >= maxY:
 				continue
-			scaled_x, scaled_y = x*self.grid_size, y*self.grid_size
+
+			scaled_x, scaled_y = (x - minX) * 32, (y - minY) * 24
 			scaledPoint = (scaled_x, scaled_y)
 			game_screen.blit(tile, scaledPoint)
